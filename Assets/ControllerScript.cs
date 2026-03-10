0f54d761bb2aa3929d442afc9c430db96894e8a6
Karnik Parashar <parakarn@shernet.sheridancollege.ca>
2026-03-10 11:48:05 -0400

    Played around a with speed and controls.

--- Assets/ControllerScript.cs
+++ Assets/ControllerScript.cs
@@ -5,7 +5,7 @@ public class ControllerScript : MonoBehaviour
 
 
 {
-    public float speed = 5;
+    public float speed = 50;
     public Vector2 movement;
     public AudioSource SFX;
     public AudioSource TFX;
@@ -17,9 +17,9 @@ public class ControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.position += (Vector3)movement * speed * Time.deltaTime; 
+        transform.position += (Vector3)movement * speed * Time.deltaTime; 
 
-        transform.position = movement;
+        //transform.position = movement;
     }
 
     public void OnMove(InputAction.CallbackContext context)
